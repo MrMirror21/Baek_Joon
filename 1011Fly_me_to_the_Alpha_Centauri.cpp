21322d1116e1da5e1ac2063922b7d6d9fccdc4f3
MrMirror21 <mylittlesentry@gmail.com>
2020-03-24 02:13:00 +0900

    24/03

--- 1011Fly_me_to_the_Alpha_Centauri.cpp
+++ 1011Fly_me_to_the_Alpha_Centauri.cpp
@@ -2,30 +2,78 @@
 #include <vector>
 
 using namespace std;
-
+int Calculate(double start, double end) {
+	double length = end - start;
+	double aim = 0;
+	int mul_times = 0;
+	if (length <= 3) return length;
+	while (aim < length) {
+		mul_times++;
+		aim += 2 * mul_times;
+	}
+	if (aim - mul_times < length) {
+		return 2 * mul_times;
+	}
+	else {
+		return 2 * mul_times - 1;
+	}
+}
+/*
 int Calculate(int start, int end) {
 	int length = end - start;
 	int jump_times = 0;
 	int sum = 0;
 	vector<int> fomula{};
+
 	if (length <= 3) {
 		jump_times = length;
 		return jump_times;
 	}
+	fomula.push_back(1);
+	sum += 3;
+	jump_times += 3;
 	while (sum < length) {
-		fomula.push_back(1);
-		sum++;
-		jump_times++;
-		if ()
+		int tail_index = fomula.size() - 1;
+		auto iter = fomula.end();
+		iter--;
+		if (*iter != 2) {
+			fomula[tail_index]++;
+			sum++;
+		}
+		else{
+			for (int i = tail_index; i >= 0; i--) {
+				if (i == 0) {
+					if (fomula.size() == 1) {
+						fomula.push_back(1);
+						sum++;
+						jump_times++;
+						continue;
+					}
+					fomula.push_back(1);
+					sum++;
+					jump_times++;
+					continue;
+				}
+				else if (fomula[i - 1] < i + 2) {
+					fomula[i - 1]++;
+					if (abs(fomula[i - 1] - fomula[i]) > 1 || abs(fomula[i - 1] - fomula[i]) > 1) {
+						fomula[i]--;
+						continue;
+					}
+					sum++;
+					continue;
+				}
+			}
+		}
 	}
 	return jump_times;
 }
-
+*/
 void HowMany(int userinput) {
 	int count = 0;
 	vector<int> jump_times{};
 	while (count < userinput) {
-		int start, end;
+		double start, end;
 		count++;
 		cin >> start >> end;
 		jump_times.push_back(Calculate(start, end));
--- 2775부녀회장이될테야.cpp
+++ 2775부녀회장이될테야.cpp
@@ -4,33 +4,25 @@
 using namespace std;
 
 int CalPeople(int _floor, int _roomNum) {
-	cout << "start" << endl;
 	vector<vector<int>> matrix{};
 	for (int i = 0; i < _floor + 1; i++) {
-		cout << "floor : " << i << endl;
 		vector<int> floor{};
 		if (i == 0) {
 			for (int j = 0; j < _roomNum; j++) {
 				floor.push_back(j + 1);
-				cout << floor[j] << endl;
 			}
 			matrix.push_back(floor);
 			continue;
 		}
 		for (int j = 0; j < _roomNum; j++) {
-			cout << "room : " << j << endl;
 			floor.push_back(0);
 			for (int k = 0; k <= j; k++) {
 				floor[j] += matrix[i - 1][k];
-				cout << floor[j] << endl;
 			}
 		}
 		matrix.push_back(floor);
 	}
-	cout << "end" << endl;
-	int result = matrix[_floor][_roomNum - 1];
-	int* p = &matrix[_floor][_roomNum - 1];
-	return *p;
+	return matrix[_floor][_roomNum - 1];
 }
 
 void HowMany(int userinput) {
